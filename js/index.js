bf205f8c2cec96b170423663d61262f0f84e11de
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-06-05 23:10:48 +0400

    change sizes

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@
 
 const gallery = new Swiper(".gallery", {
   direction: "horizontal",
-  initialSlide: 0,
+  slidesPerView: "auto",
   loop: true,
   speed: 3000,
   effect: "cube",
-  sentredSlides: true,
+  // sentredSlides: true,
   dynamicBullets: true,
   pagination: {
     el: ".swiper-pagination",
@@ -19,45 +19,45 @@ const gallery = new Swiper(".gallery", {
 });
 
 const swiper = new Swiper(".swiper", {
-    direction: "horizontal",
-    initialSlide: 10,
-    loop: true,
-    speed: 3000,
-    effect: "coverflow",
-    slidesPerView: 3,
-    spaceBetween: 20,
-    sentredSlides: true,
-    dynamicBullets: true,
+  direction: "horizontal",
+  initialSlide: 0,
+  loop: true,
+  speed: 3000,
+  effect: "coverflow",
+  slidesPerView: 3,
+  // spaceBetween: 20,
+  sentredSlides: true,
+  dynamicBullets: true,
 
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    autoplay: {
-        delay: 3000,
-    },
+  autoplay: {
+    delay: 3000,
+  },
 
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     320: {
-    //     slidesPerView: 1,
-        
-    //     },
-    //     // when window width is >= 480px
-    //     480: {
-    //     slidesPerView: 2,
-        
-    //     },
-    //     // when window width is >= 640px
-    //     640: {
-    //     slidesPerView: 3,
-        
-    //     },
-    // },
+  // breakpoints: {
+  //     // when window width is >= 320px
+  //     320: {
+  //     slidesPerView: 1,
+
+  //     },
+  //     // when window width is >= 480px
+  //     480: {
+  //     slidesPerView: 2,
+
+  //     },
+  //     // when window width is >= 640px
+  //     640: {
+  //     slidesPerView: 3,
+
+  //     },
+  // },
 });
